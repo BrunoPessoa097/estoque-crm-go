663d2465d352915cf61b9788157a925b0bdc9f13
Bruno Pessoa <brunopessoa097@gmail.com>
2025-11-14 01:15:55 -0300

    refactor: adicionando nas funções variaveil global de histórico

--- historico/historico.go
+++ historico/historico.go
@@ -6,21 +6,19 @@ import (
 	"github.com/brunopessoa097/estudos-geral/utils"
 )
 
-var hist = make([]utils.Historico, 0)
-
 func Add(tipo string, p *utils.Produto) {
-	hist = append(hist, utils.Historico{
+	utils.Hist = append(utils.Hist, utils.Historico{
 		Tipo:    tipo,
 		Produto: p,
 	})
 }
 
 func Listar() {
-	if len(hist) == 0 {
+	if len(utils.Hist) == 0 {
 		fmt.Println("Não há histórico")
 	}
 
-	for _, res := range hist {
+	for _, res := range utils.Hist {
 		//fmt.Println(res)
 		fmt.Printf("Tipo: %s | Nome: %s | Preço: %.2f | Estoque: %d \n", res.Tipo, res.Produto.Nome, res.Produto.Preco, res.Produto.Estoque)
 	}
