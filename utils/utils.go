c3bc25b5a75f51594f0cc752c472bd5e50be2345
Bruno Pessoa <brunopessoa097@gmail.com>
2025-11-14 01:21:54 -0300

    feat: adicionando função clear aos utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
 type Produto struct {
 	Nome    string
 	Preco   float64
@@ -15,3 +21,16 @@ var (
 	Produtos = make(map[string]Produto)
 	Hist     = make([]Historico, 0)
 )
+
+func Clear() {
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windowns" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
